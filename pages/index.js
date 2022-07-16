e4a1ac27d382f46ce431de2da4efc00404a4c1ec
Alex Lin <linalex7@protonmail.com>
2022-07-16 14:18:58 +0800

    readded link button

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
+
+import { Flex, Heading, useColorMode, useColorModeValue } from '@chakra-ui/react'
 import Link from 'next/link'
 
-const HomePage = () => (
-    <Link href="/projects">
-        <a>this page!</a>
-    </Link>
-)
+const HomePage = () => {
+    const { toggleColorMode } = useColorMode()
+    const exampleColor = useColorModeValue("gray.100", "gray.700")
+
+    return (
+    <Flex height="100vh" alignItems="center" justifyContent="center">
+        <button onClick={toggleColorMode}>Toggle Theme</button>
+        <Flex direction="column" background={exampleColor} p={12} rounded={6}>
+            <Heading mb={6}>
+                Login
+            </Heading>
+            <Link href="/projects">My Projects</Link>
+        </Flex>
+    </Flex>
+    )
+}
 
 export default HomePage
