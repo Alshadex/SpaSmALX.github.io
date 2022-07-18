40dc6f6ebb6da6610e90e7329cdb5ee74256d622
Alex Lin <linalex7@protonmail.com>
2022-07-18 16:43:01 +0800

    created basic navbar. Added resume

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,32 @@
 
 import { Flex, Heading, useColorMode, useColorModeValue } from '@chakra-ui/react'
 import Link from 'next/link'
+import Navbar from '../components/navbar'
 
 const HomePage = () => {
     const { toggleColorMode } = useColorMode()
     const exampleColor = useColorModeValue("gray.100", "gray.700")
 
     return (
-    <Flex height="100vh" alignItems="center" justifyContent="center">
-        <button onClick={toggleColorMode}>Toggle Theme</button>
-        <Flex direction="column" background={exampleColor} p={12} rounded={6}>
-            <Heading mb={6}>
-                Login
-            </Heading>
-            <Link href="/projects">My Projects</Link>
-        </Flex>
+    // <Flex height="100vh" alignItems="center" justifyContent="center">
+    //     <Navbar ></Navbar>
+    //     <button onClick={toggleColorMode}>Toggle Theme</button> */}
+    //     <Flex direction="column" background={exampleColor} p={12} rounded={6}>
+    //         <Heading mb={6}>
+    //             Login
+    //         </Heading>
+    //         <Link href="/projects">My Projects</Link>
+    //     </Flex>
+    // </Flex>
+    <Flex height="150vh" justifyContent="center">
+    
+        <Navbar>
+            <Link href="/">
+            <a>Back to home</a>
+            </Link>
+        </Navbar>
     </Flex>
+
     )
 }
 
