dbff355dc5325ff5a929a74a9d9a075ab347e3e2
Alex Lin <linalex7@protonmail.com>
2023-02-03 22:52:42 -0800

    Added bubbles animation

--- animations/solar_system.js
+++ animations/solar_system.js
@@ -1,79 +1,83 @@
-export function createP5Animation1(targetId) {
-  let center = []
-  let planetData = [
-    /* Mercury */  {Xcoef: 50, Ycoef: 22, Tcoef:4.14, size: 10, color: 'grey'},
-    /* Venus */    {Xcoef: 85, Ycoef: 38, Tcoef:1.62, size: 12, color: '#e3bb76'},
-    /* Earth */    {Xcoef: 118, Ycoef: 54, Tcoef:1, size: 15, color: '#1c821a'},
-    /* Mars  */    {Xcoef: 154, Ycoef: 70, Tcoef:0.53, size: 10, color: '#CC3333'},
-    /* Jupiter */  {Xcoef: 188, Ycoef: 86, Tcoef:0.08, size: 20, color: '#d8ca9d'},
-    /* Saturn */   {Xcoef: 222, Ycoef: 103, Tcoef:0.034, size: 18, color: '#c99039'},
-    /* Uranus */   {Xcoef: 261, Ycoef: 118, Tcoef:0.011, size: 16, color: '#4FD0E7'},
-    /* Neptune */  {Xcoef: 293, Ycoef: 133, Tcoef:0.006, size: 16, color: '#4b70dd'},
-    /* Pluto */    {Xcoef: 330, Ycoef: 148, Tcoef:0.004, size: 9, color: '#968570'},
-
-  ]
-  let planets = []
-
-
-  targetId.setup = function() {
-    createCanvas(700, 393);
-    center = [Math.floor(width/2), Math.floor(height/2)]
-    for (i = 0; i < planetData.length; i++){
-      planets.push(new Planet(planetData[i].Xcoef,planetData[i].Ycoef,planetData[i].Tcoef, planetData[i].size, planetData[i].color));
-    }
-  }
-
-  targetId.draw = function() {
-    background(0);
-    strokeWeight(0.5)
-    stroke("#ffffff");
-    fill('#000000');
-    ellipse(center[0],center[1], 660, 297);
-    ellipse(center[0],center[1], 590, 265.5);
-    ellipse(center[0],center[1], 520, 234);
-    ellipse(center[0],center[1], 450, 202.5);
-    ellipse(center[0],center[1], 380, 171);
-    ellipse(center[0],center[1], 310, 139.5);
-    ellipse(center[0],center[1], 240, 108);
-    ellipse(center[0],center[1], 170, 76.5);
-    ellipse(center[0],center[1], 100, 45);
-    fill('yellow')
-    noStroke()
-    circle(center[0], center[1], 30) // sun
-    
-    for (i = 0; i < planets.length; i++){
-      planets[i].move()
-      planets[i].display()
-    }
-  }
-
-
-  class Planet {
-    constructor(Xcoef,Ycoef,Tcoef,size,color){
-      this.x = 0
-      this.y = 0
-      this.size = size
-      this.color = color
-      
-      this.Xcoef = Xcoef
-      this.Ycoef = Ycoef
-      this.Tcoef = Tcoef
-      this.t = random(0, 2*PI)
-    }
-
-    move() {
-      this.x = this.Xcoef*sin(this.t) + Math.floor(width/2)
-      this.y = this.Ycoef*cos(this.t) + Math.floor(height/2)
-      this.t += 0.02 * this.Tcoef
-      if (this.t >= 2*PI){
-        this.t = 0
+let p5Instance;
+// Modal
+$('#solarSystemModal').on('shown.bs.modal', function (e) {
+  let sketch = function(p) {
+    class Planet {
+      constructor(Xcoef,Ycoef,Tcoef,size,color){
+        this.x = 0
+        this.y = 0
+        this.size = size
+        this.color = color
+        
+        this.Xcoef = Xcoef
+        this.Ycoef = Ycoef
+        this.Tcoef = Tcoef
+        this.t = p.random(0, 2*p.PI)
       }
-      
-    }
 
-    display() {
-      fill(this.color);   
-      circle(this.x, this.y, this.size);
+      move() {
+        this.x = this.Xcoef*p.sin(this.t) + Math.floor(p.width/2)
+        this.y = this.Ycoef*p.cos(this.t) + Math.floor(p.height/2)
+        this.t += 0.02 * this.Tcoef
+        if (this.t >= 2*p.PI){
+          this.t = 0
+        }
+        
+      }
+
+      display() {
+        p.fill(this.color);   
+        p.circle(this.x, this.y, this.size);
+      }
     }
-  }
-}
+    let center = []
+    let planetData = [
+      /* Mercury */  {Xcoef: 50, Ycoef: 22, Tcoef:4.14, size: 10, color: 'grey'},
+      /* Venus */    {Xcoef: 85, Ycoef: 38, Tcoef:1.62, size: 12, color: '#e3bb76'},
+      /* Earth */    {Xcoef: 118, Ycoef: 54, Tcoef:1, size: 15, color: '#1c821a'},
+      /* Mars  */    {Xcoef: 154, Ycoef: 70, Tcoef:0.53, size: 10, color: '#CC3333'},
+      /* Jupiter */  {Xcoef: 188, Ycoef: 86, Tcoef:0.08, size: 20, color: '#d8ca9d'},
+      /* Saturn */   {Xcoef: 222, Ycoef: 103, Tcoef:0.034, size: 18, color: '#c99039'},
+      /* Uranus */   {Xcoef: 261, Ycoef: 118, Tcoef:0.011, size: 16, color: '#4FD0E7'},
+      /* Neptune */  {Xcoef: 293, Ycoef: 133, Tcoef:0.006, size: 16, color: '#4b70dd'},
+      /* Pluto */    {Xcoef: 330, Ycoef: 148, Tcoef:0.004, size: 9, color: '#968570'},
+
+    ]
+    let planets = []
+    p.setup = function() {
+      p.createCanvas(700, 393);
+      center = [Math.floor(p.width/2), Math.floor(p.height/2)]
+      for (i = 0; i < planetData.length; i++){
+        planets.push(new Planet(planetData[i].Xcoef,planetData[i].Ycoef,planetData[i].Tcoef, planetData[i].size, planetData[i].color));
+      }
+    };
+    p.draw = function() {
+      p.background(0);
+      p.strokeWeight(0.5)
+      p.stroke("#ffffff");
+      p.fill('#000000');
+      p.ellipse(center[0],center[1], 660, 297);
+      p.ellipse(center[0],center[1], 590, 265.5);
+      p.ellipse(center[0],center[1], 520, 234);
+      p.ellipse(center[0],center[1], 450, 202.5);
+      p.ellipse(center[0],center[1], 380, 171);
+      p.ellipse(center[0],center[1], 310, 139.5);
+      p.ellipse(center[0],center[1], 240, 108);
+      p.ellipse(center[0],center[1], 170, 76.5);
+      p.ellipse(center[0],center[1], 100, 45);
+      p.fill('yellow')
+      p.noStroke()
+      p.circle(center[0], center[1], 30) // sun
+      
+      for (i = 0; i < planets.length; i++){
+        planets[i].move()
+        planets[i].display()
+      }
+    };
+  };
+  p5Instance = new p5(sketch, 'solarSystem-canvas');
+});
+
+$('#solarSystemModal').on('hidden.bs.modal', function (e) {
+  p5Instance.remove();
+});
--- animations/teleporting_bubbles.js
+++ animations/teleporting_bubbles.js
@@ -1,54 +1,66 @@
-let circles = [
-  {x: 23, y: 23, d: 15, theta: 0},
-  {x: 55, y: 55, d: 25, theta: 0},
-  {x: 55, y: 55, d: 25, theta: 0},
-  {x: 55, y: 55, d: 25, theta: 0},
-  {x: 55, y: 55, d: 25, theta: 0}
-]
+// let p5Instance;
 
-function setup(){ 
-  createCanvas(576, 324);
-  index = 0;
-  fill('red')
-  while (index < circles.length) {
-    circles[index].x = random(0, 576)
-    circles[index].y = random(0, 324)
-    circles[index].d = random(10, 80)
-    circles[index].theta = random(0, HALF_PI);
-    index++;
-  }
-}
+// Modal
+$('#bubblesModal').on('shown.bs.modal', function (e) {
+  let p5Instance;
 
-function draw() {
-  background('grey');
-  
-  index = 0;
-  fill('blue')
-  while (index < circles.length) {
-    drawingContext.shadowOffsetX = 5;
-    drawingContext.shadowOffsetY = -5;
-    drawingContext.shadowBlur = 10;
-    drawingContext.shadowColor = 'black';
-    circle(circles[index].x, circles[index].y, circles[index].d)
-    index++;
-  }
-  
-  index = 0;
-  while (index < circles.length) {
-    
-    circles[index].x += 2*cos(circles[index].theta);
-    circles[index].y += 2*sin(circles[index].theta);
-    
-    
-    if (circles[index].x >= 576){
-      circles[index].x = 0
-    } else if (circles[index].x <= 0){
-      circles[index].x = 576
-    }
-    if (circles[index].y >= 324){
-      circles[index].y = 0
-    }
-    index++;
-  }
-  
-}
+  let sketch = function(p) {
+    let index;
+    let circles = [
+      {x: 23, y: 23, d: 15, theta: 0},
+      {x: 55, y: 55, d: 25, theta: 0},
+      {x: 55, y: 55, d: 25, theta: 0},
+      {x: 55, y: 55, d: 25, theta: 0},
+      {x: 55, y: 55, d: 25, theta: 0}
+    ];
+
+    p.setup = function() {
+      p.createCanvas(576, 324);
+      index = 0;
+      p.fill('red');
+      while (index < circles.length) {
+        circles[index].x = p.random(0, 576);
+        circles[index].y = p.random(0, 324);
+        circles[index].d = p.random(10, 80);
+        circles[index].theta = p.random(0, p.HALF_PI);
+        index++;
+      }
+    };
+
+    p.draw = function() {
+      p.background('yellow');
+
+      index = 0;
+      p.fill('blue');
+      while (index < circles.length) {
+        p.drawingContext.shadowOffsetX = 5;
+        p.drawingContext.shadowOffsetY = -5;
+        p.drawingContext.shadowBlur = 10;
+        p.drawingContext.shadowColor = 'black';
+        p.circle(circles[index].x, circles[index].y, circles[index].d);
+        index++;
+      }
+
+      index = 0;
+      while (index < circles.length) {
+        circles[index].x += 2 * p.cos(circles[index].theta);
+        circles[index].y += 2 * p.sin(circles[index].theta);
+
+        if (circles[index].x >= 576) {
+          circles[index].x = 0;
+        } else if (circles[index].x <= 0) {
+          circles[index].x = 576;
+        }
+        if (circles[index].y >= 324) {
+          circles[index].y = 0;
+        }
+        index++;
+      }
+    };
+  };
+  p5Instance = new p5(sketch, 'bubbles-canvas');
+});
+
+$('#bubblesModal').on('hidden.bs.modal', function (e) {
+  p5Instance.remove();
+});
